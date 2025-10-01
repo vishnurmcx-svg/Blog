92ec634247c08744877982fcd8a8b85844d5890d
vishnurmcx-svg <vishnurmcx@gmail.com>
2025-10-02 03:21:52 +0530

    connected database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
-port = 3000
+require('dotenv').config()
+port = process.env.PORT || 5000
+
+
+// console.log(process.env)
+main().then(() => console.log('Database is running..!')).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URL);
+
+}
 
 app.listen(port, () =>{
     console.log(`port is listening on ${port}`);
